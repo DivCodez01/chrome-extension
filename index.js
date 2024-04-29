0fe1ea56ea20ca2207e3ccd70f5070dd15c945aa
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-29 15:51:08 +0200

    soundWhenYouclicked

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const saveBt = document.getElementById("save-bt");
 const tabBt = document.getElementById("tab-bt");
 const deleteBt = document.getElementById("delete-bt");
 const saveElm = document.getElementById("save-elm");
+const label = document.querySelector("label");
 
 let myLStrorage = JSON.parse(localStorage.getItem("myArr"));
 
@@ -12,17 +13,21 @@ const localStorageToArray = () => {
     myArr = myLStrorage;
 }
 
+let koko = "swsws";
+
 localStorageToArray()
 
 saveBt.addEventListener("click", inputLead);
 
 function inputLead() {
+    label.innerHTML = `<audio src="Click_-_Sound_Effect__HD_(128k).mp3" autoplay></audio>`
     if (inputElm.value) {
         myArr.push(inputElm.value);
         console.log(myArr);
     }
+
     inputElm.value = ""
-    // localStorage.setItem("myArr", JSON.stringify(myArr));
+    localStorage.setItem("myArr", JSON.stringify(myArr));
     readInputItems()
 }
 
@@ -40,27 +45,25 @@ function readInputItems() {
     saveElm.innerHTML = saveInput;
 }
 
-deleteBt.addEventListener("click", deleteElms);
+deleteBt.addEventListener("dblclick", deleteElms);
 
 function deleteElms() {
+    label.innerHTML = `<audio src="Mouse_Click_Sound_Effect(128k).mp3" autoplay></audio>`
     myArr = []
     saveElm.innerHTML = "";
 }
 
 tabBt.addEventListener("click", chromeTabsave)
 function chromeTabsave() {
+    label.innerHTML = `<audio src="Click_-_Sound_Effect__HD_(128k).mp3" autoplay></audio>`
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         myArr.push(tabs[0].url);
 
-        // localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-        // render(myLeads);
-        readInputItems()
+        inputLead()
     })
 }
 
 inputElm.addEventListener("change", function () {
-    readInputItems()
     inputLead()
 })
 
